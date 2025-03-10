65b5d9878bc6240101f1db6d0dca55cbbef39fb7
Spartan <spartan@MacBookAir.attlocal.net>
2025-03-10 16:17:12 -0700

    With js

---
+++ scripts.js
@@ -0,0 +1,52 @@
+// GSAP Scroll Animations
+gsap.from("#about", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+  ease: "power3.out",
+  scrollTrigger: {
+    trigger: "#about",
+    start: "top bottom",
+    end: "bottom top",
+    scrub: 1,
+  }
+});
+
+gsap.from("#skills", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+  ease: "power3.out",
+  scrollTrigger: {
+    trigger: "#skills",
+    start: "top bottom",
+    end: "bottom top",
+    scrub: 1,
+  }
+});
+
+gsap.from("#projects", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+  ease: "power3.out",
+  scrollTrigger: {
+    trigger: "#projects",
+    start: "top bottom",
+    end: "bottom top",
+    scrub: 1,
+  }
+});
+
+gsap.from("#contact", {
+  opacity: 0,
+  y: 100,
+  duration: 1,
+  ease: "power3.out",
+  scrollTrigger: {
+    trigger: "#contact",
+    start: "top bottom",
+    end: "bottom top",
+    scrub: 1,
+  }
+});
